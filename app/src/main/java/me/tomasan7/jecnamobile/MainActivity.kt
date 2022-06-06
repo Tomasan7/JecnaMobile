ddce12dedb1131957cf5e2f85238c6f4a26c34d3
Tomasan7 <tomashula06@gmail.com>
2022-06-06 17:27:20 +0200

    Migrate MainActivity to Material 3
    I forgot to do it :)

--- app/src/main/java/me/tomasan7/jecnamobile/MainActivity.kt
+++ app/src/main/java/me/tomasan7/jecnamobile/MainActivity.kt
@@ -1,42 +1,24 @@
 package me.tomasan7.jecnamobile
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.annotation.RequiresApi
+import androidx.compose.material3.Text
 import me.tomasan7.jecnamobile.ui.theme.JecnaMobileTheme
 
 class MainActivity : ComponentActivity()
 {
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
+
         setContent {
             JecnaMobileTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                Text("Hello World")
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String)
-{
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview()
-{
-    JecnaMobileTheme {
-        Greeting("Android")
     }
 }
