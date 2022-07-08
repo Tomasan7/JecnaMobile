67d224dfd7a10a9830b2d755a81bbeea8f51498f
Tomasan7 <tomashula06@gmail.com>
2022-07-08 11:12:03 +0200

    Fix crash on LoginScreen

--- app/src/main/kotlin/me/tomasan7/jecnamobile/screen/view/LoginScreen.kt
+++ app/src/main/kotlin/me/tomasan7/jecnamobile/screen/view/LoginScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
@@ -29,7 +30,7 @@ import me.tomasan7.jecnamobile.ui.theme.md_theme_dark_background
 @Composable
 fun LoginScreen(
     navigator: DestinationsNavigator,
-    viewModel: LoginScreenViewModel = viewModel()
+    viewModel: LoginScreenViewModel = hiltViewModel()
 )
 {
     viewModel.login.observe(LocalLifecycleOwner.current) { event ->
