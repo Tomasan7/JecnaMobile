8bccc93e7b61d89cefdd5c35ecbb08ac004d1045
Tomasan7 <tomashula06@gmail.com>
2022-07-19 11:14:45 +0200

    Create VerticalDivider composable

---
+++ app/src/main/kotlin/me/tomasan7/jecnamobile/ui/component/VerticalDivider.kt
@@ -0,0 +1,24 @@
+package me.tomasan7.jecnamobile.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun VerticalDivider(
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.onSurfaceVariant,
+    thickness: Dp = 1.dp
+)
+{
+    Box(
+        modifier.width(thickness)
+                .background(color = color)
+    )
+}
