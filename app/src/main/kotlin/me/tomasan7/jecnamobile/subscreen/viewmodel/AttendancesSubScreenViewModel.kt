2d8c168ebc56d41140d4b9185d5fe6ed23dba79f
Tomasan7 <tomashula06@gmail.com>
2022-09-01 15:56:47 +0200

    Handle Attendances loading errors

--- app/src/main/kotlin/me/tomasan7/jecnamobile/subscreen/viewmodel/AttendancesSubScreenViewModel.kt
+++ app/src/main/kotlin/me/tomasan7/jecnamobile/subscreen/viewmodel/AttendancesSubScreenViewModel.kt
@@ -1,22 +1,27 @@
 package me.tomasan7.jecnamobile.subscreen.viewmodel
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import me.tomasan7.jecnaapi.repository.AttendancesRepository
 import me.tomasan7.jecnaapi.util.SchoolYear
-import java.time.LocalDate
+import me.tomasan7.jecnamobile.R
 import java.time.Month
 import javax.inject.Inject
 
 @HiltViewModel
 class AttendancesSubScreenViewModel @Inject constructor(
-    private val attendancesRepository: AttendancesRepository
+    private val attendancesRepository: AttendancesRepository,
+    @ApplicationContext
+    private val appContext: Context
 ) : ViewModel()
 {
     var uiState by mutableStateOf(AttendancesSubScreenState())
@@ -48,9 +53,17 @@ class AttendancesSubScreenViewModel @Inject constructor(
         loadAttendancesJob?.cancel()
 
         loadAttendancesJob = viewModelScope.launch {
-            val attendances = attendancesRepository.queryAttendancesPage(uiState.selectedSchoolYear, uiState.selectedMonth.value)
-
-            uiState = uiState.copy(loading = false, attendancesPage = attendances)
+            try
+            {
+                val attendances = attendancesRepository.queryAttendancesPage(uiState.selectedSchoolYear, uiState.selectedMonth.value)
+                uiState = uiState.copy(loading = false, attendancesPage = attendances)
+            }
+            catch (e: Exception)
+            {
+                e.printStackTrace()
+                Toast.makeText(appContext, appContext.getString(R.string.attendance_load_error), Toast.LENGTH_LONG).show()
+                uiState = uiState.copy(loading = false)
+            }
         }
     }
 }
