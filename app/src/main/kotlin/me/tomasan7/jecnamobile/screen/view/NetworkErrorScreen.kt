c493765f507be1ade809c5a46daa0c39df58419c
Tomasan7 <tomashula06@gmail.com>
2022-09-04 19:34:47 +0200

    Make NetworkErrorScreen look a little better

--- app/src/main/kotlin/me/tomasan7/jecnamobile/screen/view/NetworkErrorScreen.kt
+++ app/src/main/kotlin/me/tomasan7/jecnamobile/screen/view/NetworkErrorScreen.kt
@@ -2,15 +2,21 @@ package me.tomasan7.jecnamobile.screen.view
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.SentimentVeryDissatisfied
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
+import me.tomasan7.jecnamobile.R
 
 @RequiresApi(Build.VERSION_CODES.S)
 @RootNavGraph
@@ -19,6 +25,23 @@ import com.ramcosta.composedestinations.annotation.RootNavGraph
 fun NetworkErrorScreen()
 {
     Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("Network error", color = MaterialTheme.colorScheme.onBackground)
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(horizontal = 30.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.SentimentVeryDissatisfied,
+                contentDescription = "Sad smiley face",
+                tint = MaterialTheme.colorScheme.onBackground,
+                modifier = Modifier.size(150.dp)
+            )
+            Spacer(Modifier.height(20.dp))
+            Text(
+                text = stringResource(R.string.error_no_internet_connection),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.headlineSmall,
+                color = MaterialTheme.colorScheme.onBackground,
+            )
+        }
     }
 }
