5587d0cc23eeb6ca5666d47fb90c65beaaef1188
Tomasan7 <tomashula06@gmail.com>
2022-09-20 09:37:02 +0100

    Update gradle dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,7 +77,7 @@ dependencies {
     val composeVersion = rootProject.extra["compose_version"]
     val lifecycleVersion = "2.5.1"
 
-    implementation("androidx.activity:activity-compose:$composeVersion")
+    implementation("androidx.activity:activity-compose:1.5.1")
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
     debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
