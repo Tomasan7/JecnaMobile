3f1a39ae14313d0358f82cdbc18a083c0a2ee3a7
Tomasan7 <tomashula06@gmail.com>
2022-10-03 20:23:51 +0200

    Remove unused dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -110,6 +110,5 @@ dependencies {
     implementation("com.github.ireward:compose-html:1.0.2")
 
     implementation("androidx.datastore:datastore:1.0.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
 }
