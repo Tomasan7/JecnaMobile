01404d582b1b850d32763dd2a1ee3d59218552e4
Tomasan7 <tomashula06@gmail.com>
2023-01-22 14:19:02 +0100

    Clean gradle build scripts and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,10 @@
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("com.google.devtools.ksp") version "1.7.10-1.0.6"
-    kotlin("plugin.serialization") version "1.7.10"
-
-    kotlin("kapt")
-    id("dagger.hilt.android.plugin")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.kotlin)
+    alias(libs.plugins.serialization)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kapt)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -46,7 +45,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.3.1"
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
     }
 
     packagingOptions {
@@ -70,45 +69,26 @@ kapt {
 }
 
 dependencies {
-    /* Main JecnaAPI dependency. */
-    implementation("me.tomasan7:jecna-api:1.2.11")
+    implementation(libs.jecnaAPI)
 
-    /* --- Jetpack compose --- */
-    val composeVersion = rootProject.extra["compose_version"]
-    val lifecycleVersion = "2.5.1"
+    implementation(libs.compose.material3)
+    implementation(libs.compose.material.icons.extended)
 
-    implementation("androidx.activity:activity-compose:1.6.0")
-    implementation("androidx.compose.ui:ui:$composeVersion")
-    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
-    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    implementation(libs.activity.compose)
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.5.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
+    implementation(libs.composeHtml)
 
-    val composeMaterial3Version = "1.0.0-beta02"
-    implementation("androidx.compose.material3:material3:$composeMaterial3Version")
-    implementation("androidx.compose.material3:material3-window-size-class:$composeMaterial3Version")
+    implementation(libs.composeDestinations.core)
+    ksp(libs.composeDestinations.ksp)
 
-    val accompanistVersion = "0.25.1"
-    implementation("com.google.accompanist:accompanist-swiperefresh:$accompanistVersion")
-    implementation("com.google.accompanist:accompanist-flowlayout:$accompanistVersion")
-    implementation("com.google.accompanist:accompanist-systemuicontroller:$accompanistVersion")
+    implementation(libs.accompanist.swiperefresh)
+    implementation(libs.accompanist.flowlayout)
+    implementation(libs.accompanist.systemuicontroller)
 
-    implementation("androidx.compose.material:material-icons-extended:$composeVersion")
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.navigation.compose)
+    kapt(libs.hilt.android.compiler)
 
-    val composeDestinationsVersion = "1.6.20-beta"
-    implementation("io.github.raamcosta.compose-destinations:core:$composeDestinationsVersion")
-    ksp("io.github.raamcosta.compose-destinations:ksp:$composeDestinationsVersion")
-
-    /* Dagger-Hilt */
-    implementation("com.google.dagger:hilt-android:2.43.2")
-    kapt("com.google.dagger:hilt-android-compiler:2.43.2")
-    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
-
-    /* Compose HTML */
-    implementation("com.github.ireward:compose-html:1.0.2")
-
-    implementation("androidx.datastore:datastore:1.0.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
+    implementation(libs.datastore)
+    implementation(libs.serialization.json)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-ext {
-    extra["compose_version"] = "1.2.1"
-}
-
-plugins {
-    id("com.android.application") version "7.2.2" apply false
-    id("com.android.library") version "7.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
-}
-
-buildscript {
-    dependencies {
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.43.2")
-    }
-}
-
-
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
