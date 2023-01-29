6ad2511a7c85e2bb8b3fc72ecb80fe634dbc1c82
Tomasan7 <tomashula06@gmail.com>
2023-01-29 20:37:06 +0100

    Update MainActivity

--- app/src/main/kotlin/me/tomasan7/jecnamobile/MainActivity.kt
+++ app/src/main/kotlin/me/tomasan7/jecnamobile/MainActivity.kt
@@ -5,21 +5,76 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
-import androidx.compose.material3.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.ramcosta.composedestinations.DestinationsNavHost
 import dagger.hilt.android.AndroidEntryPoint
+import me.tomasan7.jecnamobile.login.AuthRepository
+import me.tomasan7.jecnamobile.login.NavGraphs
+import me.tomasan7.jecnamobile.login.destinations.LoginScreenDestination
+import me.tomasan7.jecnamobile.ui.theme.JecnaMobileTheme
 import me.tomasan7.jecnamobile.util.*
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity()
 {
+    @Inject
+    lateinit var authRepository: AuthRepository
+
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
 
-        setContent {
-            Text("Hello World!", color = Color.White)
+        val startDestination = if (authRepository.exists())
+        {
+            /* TODO: Make it MainScreen */
+            showLongToast("Navigation to MainScreen placeholder...")
+            return
         }
+        else
+            LoginScreenDestination
+
+        setContent {
+            val settings by settingsAsStateAwaitFirst()
+            val isAppInDarkTheme = isAppInDarkTheme(settings)
+
+            JecnaMobileTheme(isAppInDarkTheme) {
+                val backgroundColor = MaterialTheme.colorScheme.background
+
+                SystemBarColors(isAppInDarkTheme, backgroundColor)
+
+                DestinationsNavHost(
+                    navGraph = NavGraphs.root,
+                    startRoute = startDestination,
+                    modifier = Modifier.fillMaxSize().background(backgroundColor)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun SystemBarColors(isAppInDarkTheme: Boolean, color: Color = MaterialTheme.colorScheme.background)
+{
+    // https://google.github.io/accompanist/systemuicontroller/
+    val systemUiController = rememberSystemUiController()
+
+    DisposableEffect(isAppInDarkTheme) {
+        systemUiController.setSystemBarsColor(
+            color = color,
+            isNavigationBarContrastEnforced = false,
+            darkIcons = !isAppInDarkTheme
+        )
+        // setStatusBarColor() and setNavigationBarColor() also exist
+        onDispose {}
     }
 }
--- app/src/main/kotlin/me/tomasan7/jecnamobile/util/Settings.kt
+++ app/src/main/kotlin/me/tomasan7/jecnamobile/util/Settings.kt
@@ -1,11 +1,12 @@
 package me.tomasan7.jecnamobile.util
 
 import kotlinx.serialization.Serializable
+import me.tomasan7.jecnamobile.login.destinations.LoginScreenDestination
 
 @Serializable
 data class Settings(
     val theme: Theme = Theme.SYSTEM,
-    val openSubScreenRoute: String = TODO("Provide a default value for openSubScreenRoute"),
+    val openSubScreenRoute: String = LoginScreenDestination.route,
 )
 
 enum class Theme
