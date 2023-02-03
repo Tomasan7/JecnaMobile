d138d8b1fc6d0acf08b2ec4826f68f854804aa95
Tomasan7 <tomashula06@gmail.com>
2023-02-03 10:32:51 +0100

    Add CacheGradesRepository class

---
+++ app/src/main/kotlin/me/tomasan7/jecnamobile/grades/CacheGradesRepository.kt
@@ -0,0 +1,55 @@
+package me.tomasan7.jecnamobile.grades
+
+import android.content.Context
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromStream
+import me.tomasan7.jecnaapi.data.grade.GradesPage
+import me.tomasan7.jecnaapi.util.SchoolYear
+import me.tomasan7.jecnaapi.util.SchoolYearHalf
+import me.tomasan7.jecnamobile.util.CachedData
+import java.io.File
+import javax.inject.Inject
+
+class CacheGradesRepository @Inject constructor(
+    @ApplicationContext
+    private val appContext: Context,
+    private val gradesRepository: GradesRepository
+)
+{
+    private val cacheFile = File(appContext.filesDir, FILE_NAME)
+
+    fun isCacheAvailable() = cacheFile.exists()
+
+    fun getCachedGrades(): CachedData<GradesPage>?
+    {
+        if (!isCacheAvailable())
+            return null
+
+        return try
+        {
+            Json.decodeFromStream(cacheFile.inputStream())
+        }
+        catch (e: Exception)
+        {
+            e.printStackTrace()
+            null
+        }
+    }
+
+    suspend fun getRealGrades(): GradesPage
+    {
+        val gradesPage = gradesRepository.getGradesPage()
+        cacheFile.writeText(Json.encodeToString(CachedData(gradesPage)))
+        return gradesPage
+    }
+
+    /** Will not cache anything. */
+    suspend fun getRealGrades(schoolYear: SchoolYear, schoolYearHalf: SchoolYearHalf) = gradesRepository.getGradesPage(schoolYear, schoolYearHalf)
+
+    companion object
+    {
+        private const val FILE_NAME = "grades.json"
+    }
+}
