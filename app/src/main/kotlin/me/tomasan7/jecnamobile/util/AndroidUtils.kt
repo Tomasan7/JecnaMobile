541e452c4fb355b96f1056517768000e5e7f3a33
Tomasan7 <tomashula06@gmail.com>
2023-02-03 10:16:24 +0100

    Add createBroadcastReceiver helper function

--- app/src/main/kotlin/me/tomasan7/jecnamobile/util/AndroidUtils.kt
+++ app/src/main/kotlin/me/tomasan7/jecnamobile/util/AndroidUtils.kt
@@ -1,8 +1,15 @@
 package me.tomasan7.jecnamobile.util
 
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
 import android.widget.Toast
 
 fun Context.showShortToast(message: String) = Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 
 fun Context.showLongToast(message: String) = Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+
+fun createBroadcastReceiver(handler: BroadcastReceiver.(Context, Intent) -> Unit) = object : BroadcastReceiver()
+{
+    override fun onReceive(context: Context, intent: Intent) = handler(context, intent)
+}
