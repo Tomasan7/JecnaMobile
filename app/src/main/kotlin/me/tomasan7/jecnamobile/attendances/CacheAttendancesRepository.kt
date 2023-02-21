d28ad8d8e5684ebad6b627e3a1ae765c7f7f7759
Tomasan7 <tomashula06@gmail.com>
2023-02-21 10:46:49 +0100

    Createa CacheAttendancesRepository

---
+++ app/src/main/kotlin/me/tomasan7/jecnamobile/attendances/CacheAttendancesRepository.kt
@@ -0,0 +1,57 @@
+package me.tomasan7.jecnamobile.attendances
+
+import android.content.Context
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromStream
+import me.tomasan7.jecnaapi.data.attendance.AttendancesPage
+import me.tomasan7.jecnaapi.data.grade.GradesPage
+import me.tomasan7.jecnaapi.util.SchoolYear
+import me.tomasan7.jecnaapi.util.SchoolYearHalf
+import me.tomasan7.jecnamobile.util.CachedData
+import java.io.File
+import java.time.Month
+import javax.inject.Inject
+
+class CacheAttendancesRepository @Inject constructor(
+    @ApplicationContext
+    private val appContext: Context,
+    private val attendancesRepository: AttendancesRepository
+)
+{
+    private val cacheFile = File(appContext.filesDir, FILE_NAME)
+
+    fun isCacheAvailable() = cacheFile.exists()
+
+    fun getCachedAttendances(): CachedData<AttendancesPage>?
+    {
+        if (!isCacheAvailable())
+            return null
+
+        return try
+        {
+            Json.decodeFromStream(cacheFile.inputStream())
+        }
+        catch (e: Exception)
+        {
+            e.printStackTrace()
+            null
+        }
+    }
+
+    suspend fun getRealAttendances(): AttendancesPage
+    {
+        val gradesPage = attendancesRepository.getAttendancesPage()
+        cacheFile.writeText(Json.encodeToString(CachedData(gradesPage)))
+        return gradesPage
+    }
+
+    /** Will not cache anything. */
+    suspend fun getRealAttendances(schoolYear: SchoolYear, month: Month) = attendancesRepository.getAttendancesPage(schoolYear, month)
+
+    companion object
+    {
+        private const val FILE_NAME = "attendances.json"
+    }
+}
