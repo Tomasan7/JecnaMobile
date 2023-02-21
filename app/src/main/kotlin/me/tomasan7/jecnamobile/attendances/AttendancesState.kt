cf3f1dbb20f6670fa27a0a70e7165948e56f6d58
Tomasan7 <tomashula06@gmail.com>
2023-02-21 10:47:08 +0100

    Create attendances subscreen

---
+++ app/src/main/kotlin/me/tomasan7/jecnamobile/attendances/AttendancesState.kt
@@ -0,0 +1,21 @@
+package me.tomasan7.jecnamobile.attendances
+
+import de.palm.composestateevents.StateEventWithContent
+import de.palm.composestateevents.consumed
+import me.tomasan7.jecnaapi.data.attendance.AttendancesPage
+import me.tomasan7.jecnaapi.util.SchoolYear
+import java.time.Instant
+import java.time.LocalDate
+import java.time.Month
+
+data class AttendancesState(
+    val loading: Boolean = false,
+    val attendancesPage: AttendancesPage? = null,
+    val lastUpdateTimestamp: Instant? = null,
+    val selectedSchoolYear: SchoolYear = attendancesPage?.selectedSchoolYear ?: SchoolYear.current(),
+    val selectedMonth: Month = attendancesPage?.selectedMonth ?: LocalDate.now().month,
+    val snackBarMessageEvent: StateEventWithContent<String> = consumed()
+)
+{
+    val daysSorted = attendancesPage?.days?.sortedDescending()
+}
---
+++ app/src/main/kotlin/me/tomasan7/jecnamobile/attendances/AttendancesViewModel.kt
@@ -0,0 +1,169 @@
+package me.tomasan7.jecnamobile.attendances
+
+import android.content.Context
+import android.content.IntentFilter
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import de.palm.composestateevents.StateEventWithContent
+import de.palm.composestateevents.consumed
+import de.palm.composestateevents.triggered
+import io.ktor.util.network.*
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import me.tomasan7.jecnaapi.data.attendance.AttendancesPage
+import me.tomasan7.jecnaapi.util.SchoolYear
+import me.tomasan7.jecnamobile.JecnaMobileApplication
+import me.tomasan7.jecnamobile.R
+import me.tomasan7.jecnamobile.util.createBroadcastReceiver
+import java.time.*
+import java.time.format.DateTimeFormatter
+import javax.inject.Inject
+
+@HiltViewModel
+class AttendancesViewModel @Inject constructor(
+    @ApplicationContext
+    private val appContext: Context,
+    private val repository: CacheAttendancesRepository
+) : ViewModel()
+{
+    var uiState by mutableStateOf(AttendancesState())
+        private set
+
+    private var loadAttendancesJob: Job? = null
+
+    private val loginBroadcastReceiver = createBroadcastReceiver { _, _ ->
+        if (loadAttendancesJob == null || loadAttendancesJob!!.isCompleted)
+        {
+            changeUiState(snackBarMessageEvent = triggered(appContext.getString(R.string.back_online)))
+            loadReal()
+        }
+    }
+
+    fun enteredComposition()
+    {
+        loadCache()
+        loadReal()
+        appContext.registerReceiver(
+            loginBroadcastReceiver, IntentFilter(JecnaMobileApplication.SUCCESSFUL_LOGIN_ACTION)
+        )
+    }
+
+    fun leftComposition()
+    {
+        loadAttendancesJob?.cancel()
+        appContext.unregisterReceiver(loginBroadcastReceiver)
+    }
+
+    fun selectSchoolYear(schoolYear: SchoolYear)
+    {
+        changeUiState(selectedSchoolYear = schoolYear)
+        loadReal()
+    }
+
+    fun selectMonth(month: Month)
+    {
+        changeUiState(selectedMonth = month)
+        loadReal()
+    }
+
+    private fun loadCache()
+    {
+        if (!repository.isCacheAvailable())
+            return
+
+        viewModelScope.launch {
+            val cachedGrades = repository.getCachedAttendances() ?: return@launch
+
+            changeUiState(
+                attendancePage = cachedGrades.data,
+                lastUpdateTimestamp = cachedGrades.timestamp
+            )
+        }
+    }
+
+    private fun loadReal()
+    {
+        loadAttendancesJob?.cancel()
+
+        changeUiState(loading = true)
+
+        loadAttendancesJob = viewModelScope.launch {
+            try
+            {
+                val realGrades = if (isSelectedPeriodCurrent())
+                    repository.getRealAttendances()
+                else
+                    repository.getRealAttendances(uiState.selectedSchoolYear, uiState.selectedMonth)
+
+                changeUiState(
+                    attendancePage = realGrades,
+                    lastUpdateTimestamp = Instant.now()
+                )
+            }
+            catch (e: UnresolvedAddressException)
+            {
+                changeUiState(snackBarMessageEvent = triggered(getOfflineMessage()))
+            }
+            finally
+            {
+                changeUiState(loading = false)
+            }
+        }
+    }
+
+    private fun getOfflineMessage(): String
+    {
+        val cacheTimestamp = uiState.lastUpdateTimestamp
+        val localDateTime = LocalDateTime.ofInstant(cacheTimestamp, ZoneId.systemDefault())
+        val localDate = localDateTime.toLocalDate()
+
+        return if (localDate == LocalDate.now())
+        {
+            val time = localDateTime.toLocalTime()
+            val timeStr = time.format(OFFLINE_MESSAGE_TIME_FORMATTER)
+            appContext.getString(R.string.showing_offline_data_time, timeStr)
+        }
+        else
+        {
+            val dateStr = localDateTime.format(OFFLINE_MESSAGE_DATE_FORMATTER)
+            appContext.getString(R.string.showing_offline_data_date, dateStr)
+        }
+    }
+
+    fun reload() = loadReal()
+
+    private fun isSelectedPeriodCurrent() =
+        uiState.selectedSchoolYear == SchoolYear.current() && uiState.selectedMonth == LocalDate.now().month
+
+    fun onSnackBarMessageEventConsumed() = changeUiState(snackBarMessageEvent = consumed())
+
+    private fun changeUiState(
+        loading: Boolean = uiState.loading,
+        attendancePage: AttendancesPage? = uiState.attendancesPage,
+        lastUpdateTimestamp: Instant? = uiState.lastUpdateTimestamp,
+        selectedSchoolYear: SchoolYear = uiState.selectedSchoolYear,
+        selectedMonth: Month = uiState.selectedMonth,
+        snackBarMessageEvent: StateEventWithContent<String> = uiState.snackBarMessageEvent
+    )
+    {
+        uiState = uiState.copy(
+            loading = loading,
+            attendancesPage = attendancePage,
+            lastUpdateTimestamp = lastUpdateTimestamp,
+            selectedSchoolYear = selectedSchoolYear,
+            selectedMonth = selectedMonth,
+            snackBarMessageEvent = snackBarMessageEvent
+        )
+    }
+
+    companion object
+    {
+        val OFFLINE_MESSAGE_TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm")
+        val OFFLINE_MESSAGE_DATE_FORMATTER = DateTimeFormatter.ofPattern("d. M.")
+    }
+}
