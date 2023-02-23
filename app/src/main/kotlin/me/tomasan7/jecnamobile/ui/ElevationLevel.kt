656d2417631b1c4fdeb32daaf7a42f8e8918fa46
Tomasan7 <tomashula06@gmail.com>
2023-02-23 20:15:20 +0100

    Create ElevationLevel object with Material elevation levels constants

---
+++ app/src/main/kotlin/me/tomasan7/jecnamobile/ui/ElevationLevel.kt
@@ -0,0 +1,15 @@
+package me.tomasan7.jecnamobile.ui
+
+import androidx.compose.ui.unit.dp
+
+/* https://m3.material.io/styles/elevation/tokens */
+
+object ElevationLevel
+{
+    val level0 = 0.dp
+    val level1 = 1.dp
+    val level2 = 3.dp
+    val level3 = 6.dp
+    val level4 = 8.dp
+    val level5 = 12.dp
+}
