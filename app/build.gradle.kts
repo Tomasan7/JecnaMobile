92b721a6c137dc04e6b949eb22ce6085a294d890
Tomasan7 <tomashula06@gmail.com>
2023-05-10 17:58:26 +0200

    Replace deprecated method in build.gradlewith new one

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
         kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
     }
 
-    packagingOptions {
+    packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
