af9dcfee36d90d1f8750e0a5ec2d8746f3f2d585
Tomasan7 <tomashula06@gmail.com>
2023-09-19 18:48:53 +0200

    Bump version to 2.3.0 code 23

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "me.tomasan7.jecnamobile"
         minSdk = 26
         targetSdk = 34
-        versionCode = 22
-        versionName = "2.2.2"
+        versionCode = 23
+        versionName = "2.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
